8db49c393dc1cf5e989bc8a477f51b8684fc1042
CarlUdo <carl@wbsweden.com>
2024-11-05 15:56:22 +0100

    Make-inline-db
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ src/products/db.ts
@@ -0,0 +1,20 @@
+import { Categories } from './types';
+
+const categories = () : Categories[] => [
+  {
+    id: 1,
+    name: 'Food',
+  },
+  {
+    id: 2,
+    name: 'Beverages',
+  },
+  {
+    id: 3,
+    name: 'Toys',
+  },
+];
+
+export default {
+  categories,
+};
