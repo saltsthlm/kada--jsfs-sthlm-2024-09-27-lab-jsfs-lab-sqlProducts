181832cc801519ba9bbf76abe037f08e13e9c540
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-05 17:27:43 +0100

    uppdated-toutes
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ src/products/router.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { PoolClient } from 'pg';
+
+export const createRouter = (client: PoolClient) => {
+  const router = express.Router();
+
+  router.get('/', async (_req, res) => {
+    const productsList = (await client.query("SELECT * from products"))
+    res.status(200).json(productsList.rows);   
+  });
+  router.get("/:id", async (req, res) => {
+    const id = req.params.id
+    const query = "SELECT * FROM products WHERE id = $1";
+    const product = (await client.query(query,[id]))
+    res.status(200).json(product.rows[0])
+  });
+  router.post("/", async (req, res) => {
+    console.log(req.body)
+    const { name, description, price, category_id } = req.body
+    const query = "INSERT INTO Products(name, description, price, category_id) values($1, $2, $3, $4)"
+    const product = (await client.query(query, [name, description, price, category_id]))
+    console.log(product)
+    res.status(201).json(product.rows[0])
+  });
+
+  return router;  
+};
+
+
+
+
+// GET /api/products - list all products, including category name for each product
+// GET /api/products/:id - get one, including category name
+// POST /api/products - add a new product, with all fields except id. Also, require a categoryId value as parameter. You can assume that the clients knows an existing categoryId
+// PUT /api/products/:id - updates the product information with the given id
+// DEL /api/products/:id - delete the product with the given id
+// GET /api/products/?category=CATEGORY_NAME - get all products for the CATEGORY_NAME product category
