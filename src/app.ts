8bb647d14ea2e161180c751b26e86930fdb5801b
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-05 17:27:00 +0100

    moved-comments-added-express.json
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express, { Express } from 'express';
-import { createRouter } from './products';
-import { createDatabase } from './products/repository';
+import { createRouter } from './products/router';
+import { createDatabase } from './products/db';
 
 const app: Express = express();
 const port = 3001;
 
 (async () => {
   try {
-
     const client = await createDatabase();
+    app.use(express.json())
     app.use('/api/products', createRouter(client));
     app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
   } catch (error) {
@@ -16,11 +16,3 @@ const port = 3001;
     process.exit(1);
   }
 })();
-
-
-// GET /api/products - list all products, including category name for each product
-// GET /api/products/:id - get one, including category name
-// POST /api/products - add a new product, with all fields except id. Also, require a categoryId value as parameter. You can assume that the clients knows an existing categoryId
-// PUT /api/products/:id - updates the product information with the given id
-// DEL /api/products/:id - delete the product with the given id
-// GET /api/products/?category=CATEGORY_NAME - get all products for the CATEGORY_NAME product category
